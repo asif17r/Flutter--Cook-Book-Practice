4d7e3cdbf5f4ff2daeb87265520ed2b463930b7b
Asifur Rahman (Ashiq) <57610730+asif17r@users.noreply.github.com>
2023-10-02 15:28:10 +0600

    Folder Remap

---
+++ android/app/src/main/kotlin/com/example/flutter_cookbook_prac/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_cookbook_prac
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- flutter_cookbook_prac/android/app/src/main/kotlin/com/example/flutter_cookbook_prac/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_cookbook_prac
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
